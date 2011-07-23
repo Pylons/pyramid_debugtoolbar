646b5973b6f86dfbfd9da9bb84aa770d77e9c185
Chris McDonough <chrism@plope.com>
2011-07-23 14:15:16 -0400

    no sense in this living in a separate file

--- pyramid_debugtoolbar/__init__.py
+++ pyramid_debugtoolbar/__init__.py
@@ -1,5 +1,3 @@
-from pyramid_debugtoolbar.toolbar import DebugToolbar
-
 from pyramid.util import DottedNameResolver
 from pyramid.exceptions import ConfigurationError
 from pyramid.settings import asbool
@@ -19,6 +17,26 @@ def replace_insensitive(string, target, replacement):
         return string[:index] + replacement + string[index + len(target):]
     else: # no results so return the original string
         return string
+
+class DebugToolbar(object):
+    def __init__(self, request):
+        self.request = request
+        self.panels = []
+        panel_classes = self.request.registry.settings['debugtoolbar.classes']
+        activated = self.request.cookies.get('fldt_active', '').split(';')
+        for panel_class in panel_classes:
+            panel_instance = panel_class(vars={})
+            if panel_instance.dom_id() in activated:
+                panel_instance.is_active = True
+            self.panels.append(panel_instance)
+
+    def render_toolbar(self):
+        request = self.request
+        static_path = request.static_url('pyramid_debugtoolbar:static/')
+        vars = {'panels': self.panels,
+                'static_path':static_path}
+        return render('pyramid_debugtoolbar:templates/base.jinja2',
+                      vars, request=request)
 
 class DebugToolbarSubscriber(object):
     _redirect_codes = [301, 302, 303, 304]
