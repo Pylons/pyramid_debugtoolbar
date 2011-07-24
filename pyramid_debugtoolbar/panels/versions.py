0c0663623a7e29629def8463f2f5896ae917f984
Chris McDonough <chrism@plope.com>
2011-07-24 04:29:54 -0400

    show all version data intead of just Pyramid version data

--- pyramid_debugtoolbar/panels/versions.py
+++ pyramid_debugtoolbar/panels/versions.py
@@ -1,4 +1,7 @@
+import sys
 import pkg_resources
+from operator import itemgetter
+import platform
 
 from pyramid_debugtoolbar.panels import DebugPanel
 
@@ -11,13 +14,10 @@ class VersionDebugPanel(DebugPanel):
     Panel that displays the Pyramid version.
     """
     name = 'Version'
-    has_content = False
+    has_content = True
 
     def nav_title(self):
         return _('Versions')
-
-    def nav_subtitle(self):
-        return 'Pyramid %s' % pyramid_version
 
     def url(self):
         return ''
@@ -26,6 +26,17 @@ class VersionDebugPanel(DebugPanel):
         return _('Versions')
 
     def content(self):
-        return None
+        packages = []
+        for distribution in pkg_resources.working_set:
+            name = distribution.project_name
+            packages.append({'version':distribution.version,
+                             'lowername':name.lower(),
+                             'name':name})
+        plat = 'Python %s on %s' % (sys.version, platform.platform())
+        vars = {'platform':plat,
+                'packages':sorted(packages, key=itemgetter('lowername'))}
+        return self.render(
+            'pyramid_debugtoolbar.panels:templates/versions.jinja2',
+            vars, self.request)
 
 
