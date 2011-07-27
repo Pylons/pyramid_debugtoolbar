cb512001e37a0ad5e0fe70085975431a9c4a66f8
Chris McDonough <chrism@plope.com>
2011-07-27 13:22:37 -0400

    if you dont have sqla, dont barf

--- pyramid_debugtoolbar/panels/sqla.py
+++ pyramid_debugtoolbar/panels/sqla.py
@@ -2,40 +2,41 @@ import time
 
 from pyramid.threadlocal import get_current_registry
 from pyramid_debugtoolbar.panels import DebugPanel
-from pyramid_debugtoolbar.utils import format_fname
 from pyramid_debugtoolbar.utils import format_sql
 
-from sqlalchemy import event
-from sqlalchemy.pool import Pool
-from sqlalchemy.engine.base import Engine
+try:
+    from sqlalchemy import event
+    from sqlalchemy.engine.base import Engine
+    @event.listens_for(Engine, "before_cursor_execute")
+    def _before_cursor_execute(conn, cursor, stmt, params, context, execmany):
+        registry = get_current_registry()
+        registry['sqla_start_timer'] = time.time()
+
+    @event.listens_for(Engine, "after_cursor_execute")
+    def _after_cursor_execute(conn, cursor, stmt, params, context, execmany):
+        stop_timer = time.time()
+        registry = get_current_registry()
+        queries = registry.get('sqla_queries', [])
+        queries.append({
+            'duration': stop_timer - registry['sqla_start_timer'],
+            'statement': stmt,
+            'parameters': params,
+            'context': context
+        })
+        registry['sqla_queries'] = queries
+        del registry['sqla_start_timer']
+    has_sqla = True
+except ImportError:
+    has_sqla = False
 
 _ = lambda x: x
-
-@event.listens_for(Engine, "before_cursor_execute")
-def _before_cursor_execute(conn, cursor, stmt, params, context, execmany):
-    registry = get_current_registry()
-    registry['sqla_start_timer'] = time.time()
-
-@event.listens_for(Engine, "after_cursor_execute")
-def _after_cursor_execute(conn, cursor, stmt, params, context, execmany):
-    stop_timer = time.time()
-    registry = get_current_registry()
-    queries = registry.get('sqla_queries', [])
-    queries.append({
-        'duration': stop_timer - registry['sqla_start_timer'],
-        'statement': stmt,
-        'parameters': params,
-        'context': context
-    })
-    registry['sqla_queries'] = queries
-    del registry['sqla_start_timer']
-
 
 class SQLADebugPanel(DebugPanel):
     """
     Panel that displays the time a response took in milliseconds.
     """
     name = 'SQLAlchemy'
+    down = not has_sqla
 
     @property
     def queries(self):
