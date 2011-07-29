334cf43251c55b24b7d4ea3e01948094b9ca7ad6
Michael Merickel <michael@merickel.org>
2011-07-29 17:39:59 -0600

    Added a new performance panel that deprecates timer and profiler panels.

---
+++ pyramid_debugtoolbar/panels/performance.py
@@ -0,0 +1,202 @@
+try:
+    import cProfile as profile
+except ImportError: # pragma: no cover
+    import profile
+try:
+    import resource
+except ImportError:
+    pass # Will fail on Win32 systems
+import pstats
+import threading
+import time
+
+from pyramid_debugtoolbar.panels import DebugPanel
+from pyramid_debugtoolbar.utils import format_fname
+
+_ = lambda x: x
+
+lock = threading.Lock()
+
+class PerformanceDebugPanel(DebugPanel):
+    """
+    Panel that looks at the performance of requests.
+
+    It will display the time a request took and, optionally, the
+    cProfile output.
+    """
+    name = 'Performance'
+    has_content = True
+
+    user_activate = True
+    stats = None
+    function_calls = None
+
+    try: # if resource module not available, don't show content panel
+        resource
+    except NameError:
+        has_resource = False
+    else:
+        has_resource = True
+
+    def __init__(self, request):
+        self.request = request
+        self.profiler = profile.Profile()
+
+    def _wrap_timer_handler(self, handler):
+        if self.has_resource:
+            def resource_timer_handler(request):
+                _start_time = time.time()
+                self._start_rusage = resource.getrusage(resource.RUSAGE_SELF)
+                try:
+                    result = handler(request)
+                except:
+                    raise
+                finally:
+                    self._end_rusage = resource.getrusage(resource.RUSAGE_SELF)
+                    self.total_time = (time.time() - _start_time) * 1000
+
+                return result
+
+            return resource_timer_handler
+
+        def noresource_timer_handler(request):
+            _start_time = time.time()
+            try:
+                result = handler(request)
+            except:
+                raise
+            finally:
+                self.total_time = (time.time() - _start_time) * 1000
+            return result
+
+        return noresource_timer_handler
+
+    def _wrap_profile_handler(self, handler):
+        if not self.is_active:
+            return handler
+
+        def profile_handler(request):
+            with lock:
+                try:
+                    result = self.profiler.runcall(handler, request)
+                except:
+                    raise
+                finally:
+                    stats = pstats.Stats(self.profiler)
+                    function_calls = []
+                    flist = stats.sort_stats('cumulative').fcn_list
+                    for func in flist:
+                        current = {}
+                        info = stats.stats[func]
+
+                        # Number of calls
+                        if info[0] != info[1]:
+                            current['ncalls'] = '%d/%d' % (info[1], info[0])
+                        else:
+                            current['ncalls'] = info[1]
+
+                        # Total time
+                        current['tottime'] = info[2] * 1000
+
+                        # Quotient of total time divided by number of calls
+                        if info[1]:
+                            current['percall'] = info[2] * 1000 / info[1]
+                        else:
+                            current['percall'] = 0
+
+                        # Cumulative time
+                        current['cumtime'] = info[3] * 1000
+
+                        # Quotient of the cumulative time divded by the number
+                        # of primitive calls.
+                        if info[0]:
+                            current['percall_cum'] = info[3] * 1000 / info[0]
+                        else:
+                            current['percall_cum'] = 0
+
+                        # Filename
+                        filename = pstats.func_std_string(func)
+                        current['filename_long'] = filename
+                        current['filename'] = format_fname(filename)
+                        function_calls.append(current)
+
+                    self.stats = stats
+                    self.function_calls = function_calls
+
+                return result
+
+        return profile_handler
+
+    def wrap_handler(self, handler):
+        handler = self._wrap_profile_handler(handler)
+        handler = self._wrap_timer_handler(handler)
+        return handler
+
+    def title(self):
+        return _('Performance')
+
+    def nav_title(self):
+        return _('Performance')
+
+    def nav_subtitle(self):
+        if self.has_resource:
+            utime = self._end_rusage.ru_utime - self._start_rusage.ru_utime
+            stime = self._end_rusage.ru_stime - self._start_rusage.ru_stime
+            return 'CPU: %0.2fms (%0.2fms)' % ((utime + stime) * 1000.0,
+                                               self.total_time)
+        return 'TOTAL: %0.2fms' % (self.total_time)
+
+    def url(self):
+        return ''
+
+    def _elapsed_ru(self, name):
+        return getattr(self._end_rusage, name) - getattr(self._start_rusage,
+                                                         name)
+
+    def content(self):
+        utime = 1000 * self._elapsed_ru('ru_utime')
+        stime = 1000 * self._elapsed_ru('ru_stime')
+        vcsw = self._elapsed_ru('ru_nvcsw')
+        ivcsw = self._elapsed_ru('ru_nivcsw')
+        minflt = self._elapsed_ru('ru_minflt')
+        majflt = self._elapsed_ru('ru_majflt')
+
+# these are documented as not meaningful under Linux.  If you're running BSD
+# feel free to enable them, and add any others that I hadn't gotten to before
+# I noticed that I was getting nothing but zeroes and that the docs agreed. :-(
+#
+#        blkin = self._elapsed_ru('ru_inblock')
+#        blkout = self._elapsed_ru('ru_oublock')
+#        swap = self._elapsed_ru('ru_nswap')
+#        rss = self._end_rusage.ru_maxrss
+#        srss = self._end_rusage.ru_ixrss
+#        urss = self._end_rusage.ru_idrss
+#        usrss = self._end_rusage.ru_isrss
+
+        # TODO l10n on values
+        rows = (
+            (_('User CPU time'), '%0.3f msec' % utime),
+            (_('System CPU time'), '%0.3f msec' % stime),
+            (_('Total CPU time'), '%0.3f msec' % (utime + stime)),
+            (_('Elapsed time'), '%0.3f msec' % self.total_time),
+            (_('Context switches'), '%d voluntary, %d involuntary' % (
+                vcsw, ivcsw)),
+#            (_('Memory use'), '%d max RSS, %d shared, %d unshared' % (
+#                rss, srss, urss + usrss)),
+#            (_('Page faults'), '%d no i/o, %d requiring i/o' % (
+#                minflt, majflt)),
+#            (_('Disk operations'), '%d in, %d out, %d swapout' % (
+#                blkin, blkout, swap)),
+        )
+        vars = {
+            'timing_rows': rows,
+        }
+        if self.is_active:
+            vars.update({
+                'stats': self.stats,
+                'function_calls': self.function_calls,
+            })
+        return self.render(
+            'pyramid_debugtoolbar.panels:templates/performance.jinja2',
+            vars, request=self.request)
+
