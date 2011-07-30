ce9a74137efee2b2dee7a842e50c94c38d2da4b9
Chris McDonough <chrism@plope.com>
2011-07-30 12:42:25 -0400

    add a test that the version panel shows

--- demo/test.py
+++ demo/test.py
@@ -51,6 +51,14 @@ class PageTest(unittest.TestCase):
         browser.fire_event("css=.console-icon", "click")
         self.failUnless(browser.is_element_present("css=.console"))
 
+    def test_version_panel(self):
+        browser.open('/')
+        browser.wait_for_page_to_load("30000")
+        browser.fire_event("css=a#flShowToolbarButton", 'click')
+        browser.fire_event("css=a.flDebugVersionPanel", 'click')
+        result = browser.is_visible('css=#flDebugVersionPanel-content')
+        self.failUnless(result)
+
 if __name__ == '__main__':
     setUpModule()
     try:
