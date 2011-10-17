fe88fa800c3cccd43a246d31f7e95223a1d8422f
Blaise Laflamme <blaise@laflamme.org>
2011-10-17 00:02:45 -0400

    fixed docs conf typos

--- docs/conf.py
+++ docs/conf.py
@@ -105,7 +105,7 @@ pygments_style = 'sphinx'
 sys.path.append(os.path.abspath('_themes'))
 html_theme_path = ['_themes']
 html_theme = 'pyramid'
-html_theme_options = dict(github_url='http://github.com/Pylons/pyramid_debugtoolbar')
+html_theme_options = dict(github_url='https://github.com/Pylons/pyramid_debugtoolbar')
 
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
