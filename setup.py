3e61000a72bba4e0207fd1ab72622e2a1cd36bf3
Chris McDonough <chrism@plope.com>
2011-11-12 17:46:53 -0500

    prep for 0.9.5

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ install_requires = [
     ]
 
 setup(name='pyramid_debugtoolbar',
-      version='0.9.4',
+      version='0.9.5',
       description=('A package which provides an interactive HTML debugger '
                    'for Pyramid application development'),
       long_description=README + '\n\n' + CHANGES,
