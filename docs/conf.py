6036b3cc8b80abb31831184072f6aea172dac3af
Chris McDonough <chrism@plope.com>
2012-02-22 20:54:25 -0500

    prep for 0.9.9.1

--- docs/conf.py
+++ docs/conf.py
@@ -62,7 +62,7 @@ copyright = '2012, Agendaless Consulting <chrism@plope.com>'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.9.9'
+version = '0.9.9.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
