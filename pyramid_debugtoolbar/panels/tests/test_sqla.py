71ec1e54b14b3c9fa213569566096fcda0bddc85
Charlie Clark <charlieclark@fuchsia.local>
2012-03-15 01:33:27 +0100

    Added support for actually setting the maxlength

--- pyramid_debugtoolbar/panels/tests/test_sqla.py
+++ pyramid_debugtoolbar/panels/tests/test_sqla.py
@@ -3,24 +3,24 @@ import unittest
 from pyramid import testing
 
 class TestSQLAPanel(unittest.TestCase):
-    
+
     def setUp(self):
         self.request = testing.DummyRequest()
         self.config = testing.setUp(request=self.request)
 
     def tearDown(self):
         testing.tearDown()
-        
+
     def _makeOne(self):
-        from pyramid_debugtoolbar.panels.sqla import SQLADebugPanel         
+        from pyramid_debugtoolbar.panels.sqla import SQLADebugPanel
         return SQLADebugPanel(self.request)
-        
+
     def test_max_queries(self):
         panel = self._makeOne()
         self.assertEqual(panel.max_queries, None)
         self.config.registry.settings['debugtoolbar.sqla_max_queries'] = '10'
         self.assertEqual(panel.max_queries, '10')
-        
+
     def test_nav_subtitle(self):
         panel =  self._makeOne()
         self.assertEqual(panel.nav_subtitle(), None)
@@ -28,25 +28,38 @@ class TestSQLAPanel(unittest.TestCase):
         self.assertEqual(panel.nav_subtitle(), '1 query ')
         self.config.registry.settings['debugtoolbar.sqla_max_queries'] = '10'
         self.assertEqual(panel.nav_subtitle(), '1 query (10 max)')
-        
+
 
 class Test_after_cursor_execute(unittest.TestCase):
-    
+
     def setUp(self):
         self.request = testing.DummyRequest()
         self.config = testing.setUp(request=self.request)
 
     def tearDown(self):
-        testing.tearDown()        
-        
-    def test_it(self):
+        testing.tearDown()
+
+    def _makeOne(self):
         from pyramid_debugtoolbar.panels.sqla import _after_cursor_execute
         _after_cursor_execute(DummyConnection(), None, None, None, None, None)
+
+    def test_no_max_queries(self):
+        self._makeOne()
+        self.assertEqual(self.request.registry.settings.get(
+            'debugtoolbar.sqla_max_queries'), None)
+        self.assertEqual(self.request.pdtb_sqla_queries.maxlen, None)
+
+    def test_max_queries(self):
+        self.request.registry.settings['debugtoolbar.sqla_max_queries'] = '10'
+        self.assertEqual(self.request.registry.settings.get(
+            'debugtoolbar.sqla_max_queries'), '10')
+        self._makeOne()
+        self.assertEqual(self.request.pdtb_sqla_queries.maxlen, 10)
 
 
 class DummyConnection(object):
     """Dummy Database Connection"""
-    
+
     def __init__(self):
         import time
         self.pdtb_start_timer = time.time()
