437033d914ab40804fc85f117265adb4714afb88
Blaise Laflamme <blaise@laflamme.org>
2012-04-18 19:46:42 -0400

    cleaned up js code

--- demo/static/main.js
+++ demo/static/main.js
@@ -1,15 +1,15 @@
 
 require.config({
-	paths: {
-		"jquery": "jquery-1.7.2.min",
-		"toolbar": "/_debug_toolbar/static/js/toolbar"
-	}
+  paths: {
+    "jquery": "jquery-1.7.2.min",
+    "toolbar": "/_debug_toolbar/static/js/toolbar"
+  }
 });
 
 require(["jquery", "toolbar"], function($, toolbar) {
-    $(function() {
-        console.log('TEST REQUIREJS');
-    });
+  $(function() {
+    console.log('TEST REQUIREJS');
+  });
 });
 
 define("main", function(){});
