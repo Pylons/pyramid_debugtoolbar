91597b162353a1bc15b8f294ee81fdf4713b04ff
Blaise Laflamme <blaise@laflamme.org>
2013-04-17 17:22:01 -0400

    back to dev

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ docs_extras = [
     ]
 
 setup(name='pyramid_debugtoolbar',
-      version='1.0.6',
+      version='1.0.7dev',
       description=('A package which provides an interactive HTML debugger '
                    'for Pyramid application development'),
       long_description=README + '\n\n' + CHANGES,
