66a398f9ca0842c88233295de7804ba1677827dc
Blaise Laflamme <blaise@laflamme.org>
2013-04-17 15:24:26 -0400

    prep for 1.0.5

--- docs/conf.py
+++ docs/conf.py
@@ -62,7 +62,7 @@ copyright = '2012, Agendaless Consulting <chrism@plope.com>'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '1.0.4'
+version = '1.0.5'
 # The full version, including alpha/beta/rc tags.
 release = version
 
