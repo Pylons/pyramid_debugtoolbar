8d161e70a502b13ae504b0261c883c10b4d7bbdc
Andraz Brodnik <brodul@brodul.org>
2013-08-16 13:28:49 +0200

    Get rid of the patched requirejs.

--- pyramid_debugtoolbar/static/js/toolbar.js
+++ pyramid_debugtoolbar/static/js/toolbar.js
@@ -1,60 +1,69 @@
-pyramid_debugtoolbar_require.config({
-  paths: {
-    "jquery": "jquery-1.7.2.min",
-    "tablesorter": "jquery.tablesorter.min",
+var COOKIE_NAME_ACTIVE = 'pdtb_active';
+
+
+function toggle_content(elem) {
+  if (elem.is(':visible')) {
+    elem.hide();
+  } else {
+    elem.show();
+  }
+}
+
+function toggle_active(elem) {
+    elem.toggleClass('active');
+}
+
+$(document).ready(function() {
+
+
+$("#pDebugPanels li a").click( function(event_) {
+    event_.stopPropagation();
+    $("#pDebugPanels li ").removeClass("active");
+    parent_ = $(this).parent();
+    toggle_active(parent_);
+
+    $(".panelContent").hide();
+    $("#pDebugWindow").show();
+    current = $('#pDebugWindow #' + parent_.attr('id') + '-content');
+    current.show();
+});
+
+
+$('#pDebugPanels li a .switch').click(function() {
+  console.log("borg click");
+  var $panel = $(this).parent();
+  var $this = $(this);
+  var dom_id = $panel.attr('id');
+  // Turn cookie content into an array of active panels
+  var active_str = $.cookie(COOKIE_NAME_ACTIVE);
+  var active = (active_str) ? active_str.split(';') : [];
+  active = $.grep(active, function(n,i) { return n != dom_id; });
+  if ($this.hasClass('active')) {
+    $this.removeClass('active');
+    $this.addClass('inactive');
+  }
+  else {
+    active.push(dom_id);
+    $this.removeClass('inactive');
+    $this.addClass('active');
+  }
+  if (active.length > 0) {
+    $.cookie(COOKIE_NAME_ACTIVE, active.join(';'), {
+        path: '/', expires: 10
+    });
+  }
+  else {
+    $.cookie(COOKIE_NAME_ACTIVE, null, {
+      path: '/', expires: -1
+    });
   }
 });
 
-pyramid_debugtoolbar_require([
-    "jquery",
-    "tablesorter"], function($, tablesorter) {
+$(".pDebugSortable").tablesorter();
 
-      $(function() {
+current = $('#pDebugWindow #' + 'pDebugVersionPanel' + '-content');
+current.show();
+$('li#pDebugVersionPanel').addClass('active');
 
-      var pdtb = {
+});
 
-          events: {
-            ready: []
-          },
-
-      init: function() {
-
-      $("#pDebugPanels li a").click( function() {
-          $("#pDebugPanels li ").removeClass("active");
-          parent_ = $(this).parent();
-          pdtb.toggle_active(parent_);
-
-          $(".panelContent").hide();
-          $("#pDebugWindow").show();
-          current = $('#pDebugWindow #' + parent_.attr('id') + '-content');
-          current.show();
-      });
-
-      },
-
-      toggle_content: function(elem) {
-        if (elem.is(':visible')) {
-          elem.hide();
-        } else {
-          elem.show();
-        }
-      },
-
-      toggle_active: function(elem) {
-          elem.toggleClass('active');
-      },
-
-      };
-        $(document).ready(function() {
-          pdtb.init();
-          $(".pDebugSortable").tablesorter();
-
-          current = $('#pDebugWindow #' + 'pDebugVersionPanel' + '-content');
-          current.show();
-          $('li#pDebugVersionPanel').addClass('active');
-
-        });
-      });
-
-      $.noConflict(true);
-    });
