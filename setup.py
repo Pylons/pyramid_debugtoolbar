114ca759ffe34319fb784fac65ec00e4d7ea6c5d
Chris McDonough <chrism@plope.com>
2014-02-10 19:57:28 -0500

    require pyramid_mako 0.3.1 or better; it fails without.  delete duplicate requirement

--- setup.py
+++ setup.py
@@ -25,8 +25,7 @@ except IOError:
 
 install_requires = [
     'pyramid>=1.2dev',
-    'pyramid_mako>=0.3',
-    'pyramid_mako',
+    'pyramid_mako>=0.3.1', # lazy configuration loading works
     'pyramid_beaker',
     'Pygments',
     'repoze.lru',
