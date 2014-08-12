890a7ecc7a489211856c5b1f5fce1bbb8b1131d0
Michael Merickel <michael@merickel.org>
2014-08-12 13:44:19 -0500

    prep 2.2

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ docs_extras = [
     ]
 
 setup(name='pyramid_debugtoolbar',
-      version='2.1',
+      version='2.2',
       description=('A package which provides an interactive HTML debugger '
                    'for Pyramid application development'),
       long_description=README + '\n\n' + CHANGES,
