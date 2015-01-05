ae0bc878e1b13102664f3b8de60205279e22420d
jonathan vanasco <jonathan@2xlp.com>
2015-01-05 14:18:12 -0500

    working merge

--- pyramid_debugtoolbar/__init__.py
+++ pyramid_debugtoolbar/__init__.py
@@ -5,29 +5,18 @@ except ImportError: # pragma: no cover
     from pyramid.interfaces import NO_PERMISSION_REQUIRED
 from pyramid.settings import asbool
 from pyramid.wsgi import wsgiapp2
-<<<<<<< HEAD
-from pyramid_debugtoolbar.utils import as_cr_separated_list
-from pyramid_debugtoolbar.utils import as_display_debug_or_false
-from pyramid_debugtoolbar.utils import as_globals_list
-from pyramid_debugtoolbar.utils import as_int
-from pyramid_debugtoolbar.utils import as_list
-from pyramid_debugtoolbar.utils import EXC_ROUTE_NAME
-from pyramid_debugtoolbar.utils import ROOT_ROUTE_NAME
-from pyramid_debugtoolbar.utils import SETTINGS_PREFIX
-from pyramid_debugtoolbar.utils import STATIC_PATH
-=======
 from pyramid_debugtoolbar.utils import (
     as_cr_separated_list,
     as_display_debug_or_false,
     as_globals_list,
+    as_int,
     as_list,
     as_verbatim,
+    EXC_ROUTE_NAME,
+    ROOT_ROUTE_NAME,
     SETTINGS_PREFIX,
     STATIC_PATH,
-    ROOT_ROUTE_NAME,
-    EXC_ROUTE_NAME,
 )
->>>>>>> 4f312249748a86e616bb7e32cc785bbbb98c9d80
 from pyramid_debugtoolbar.toolbar import (IRequestAuthorization,
                                           toolbar_tween_factory)  # API
 toolbar_tween_factory = toolbar_tween_factory  # pyflakes
@@ -54,22 +43,18 @@ default_hosts = ('127.0.0.1', '::1')
 
 default_settings = [
     # name, convert, default
-    ('button_style', None, ''), 
     ('enabled', asbool, 'true'),
-    ('exclude_prefixes', as_cr_separated_list, []),
-    ('extra_global_panels', as_globals_list, ()),
-    ('extra_panels', as_globals_list, ()),
-    ('global_panels', as_globals_list, default_global_panel_names),
-    ('hosts', as_list, default_hosts),
-<<<<<<< HEAD
     ('intercept_exc', as_display_debug_or_false, 'debug'),
     ('intercept_redirects', asbool, 'false'),
+    ('panels', as_globals_list, default_panel_names),
+    ('extra_panels', as_globals_list, ()),
+    ('global_panels', as_globals_list, default_global_panel_names),
+    ('extra_global_panels', as_globals_list, ()),
+    ('hosts', as_list, default_hosts),
+    ('exclude_prefixes', as_cr_separated_list, []),
+    ('button_style', None, ''), 
     ('max_request_history', as_int, 100),
     ('max_visible_requests', as_int, 10),
-    ('panels', as_globals_list, default_panel_names),
-=======
-    ('exclude_prefixes', as_cr_separated_list, ()),
->>>>>>> 4f312249748a86e616bb7e32cc785bbbb98c9d80
 ]
 
 # We need to transform these from debugtoolbar. to pyramid. in our
