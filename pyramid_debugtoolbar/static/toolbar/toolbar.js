691baf668fc5fabd4b1d63c24f19e02b4f34b4ec
jonathan vanasco <jonathan@2xlp.com>
2016-12-05 13:32:54 -0500

    removed the toggle option for the toolbar settings; extended the 'panel not found' behavior for failover scenarios added a custom logging factory for development

--- pyramid_debugtoolbar/static/toolbar/toolbar.js
+++ pyramid_debugtoolbar/static/toolbar/toolbar.js
@@ -116,62 +116,128 @@ $(function () {
 
 
 /*
+This provides for a global custom logging factory, somewhat similar to python's logging module.
+
+A logger is instantiated with two arguments: label (string)  and debugging (boolean).
+
+Log lines are prepended with a label and timestamp:
+
+	[label timestamp] message
+
+Example-
+
+	The following custom logger:
+		var logger = custom_logger_factory("debugtoolbar_stickypanel", 1);
+
+	Will create this line:
+		[debugtoolbar_stickypanel 1:13:54 PM] No cookied panel detected
+
+	If the debugging is turned off
+		var logger = custom_logger_factory("debugtoolbar_stickypanel", 0);
+
+	There will be no output for this logger.
+
+This factory allows authors to use verbose, partitioned, console logging during
+development and troubleshooting, but suppress it on deployment.
+
+*/
+function CustomLoggerFactory(label, debugging_level) {
+	if (!debugging_level){
+		return(function() {return {log: function(){} }; }());
+	}
+	return (function () {
+		var timestamp = function () {};
+		timestamp.toString = function () {
+			return "[" + label + ' ' + (new Date()).toLocaleTimeString() + "]";
+		};
+		return {log: console.log.bind(console, '%s', timestamp)};
+	})();
+}
+
+
+/*
 	Sticky Panel Functionality
 
 	The functionality can be turned on/off via the global settings tab.
-	Two cookies are used to control this feature:
-	* pdtb_sticky_panel_active - is the feature active?
+	A cookies is used to control this feature:
 	* pdtb_sticky_panel_selected - the last selected panel
 */
 $(function() {
+	// custom logger
+	var logger = CustomLoggerFactory("debugtoolbar_stickypanel", 0);
+
 	// store active panel into this cookie
-	var COOKIE_NAME_STICKYPANEL_ACTIVE = 'pdtb_sticky_panel_active';
 	var COOKIE_NAME_STICKYPANEL_SELECTED = 'pdtb_sticky_panel_selected';
+	var cookied_panel = $.cookie(COOKIE_NAME_STICKYPANEL_SELECTED);
 
-	var feature_activated = ($.cookie(COOKIE_NAME_STICKYPANEL_ACTIVE) === 'true') ? true : false;
-	var selected_panel = $.cookie(COOKIE_NAME_STICKYPANEL_SELECTED);
-
-	var switcher = $('#sticky_toolbar-switch');
-		switcher.addClass(feature_activated ? 'active' : 'inactive');
-		switcher.click(function(){
-			if (switcher.hasClass('active')){
-				feature_activated = false;
-				switcher.addClass('inactive').removeClass('active');
-			} else {
-				feature_activated = true;
-				switcher.removeClass('inactive').addClass('active');
-			}
-			// set the new cookie
-			$.cookie(COOKIE_NAME_STICKYPANEL_ACTIVE, feature_activated);
-		});
-
-	// only run the feature when activated
-	if (feature_activated){
-		if (selected_panel){
-			// activate the panel if it exists and is populated
-			console.log("Activating Debug Toolbar Panel : ", selected_panel);
-			// toolbar doesn't seem to use the normal bootstrap integration, so invoke two methods
-			// the issue is that 2 identical #ids are generated : one on the `li`, one of the `li.a`
-			var panel_tab = $("#"+selected_panel);
-			if (!panel_tab.length){
-				console.log('The toolbar panel is not on this screen');
-			} else {
-				if (panel_tab.hasClass('disabled')){
-					console.log('The toolbar panel is disabled on this view');
-				} else {
-					$(panel_tab).tab('show');
-					// handle bootstrap incompatibility by invoking the content directly
-					$("#"+selected_panel+'-content').show();
-				}
+	// helper functions
+	function show_panel(panel_tab_element, selected_panel_text){
+		// show's the panel
+		$(panel_tab_element).tab('show');
+		// handle bootstrap incompatibility by invoking the content directly
+		$("#"+selected_panel_text+'-content').show();
+	}
+	function get_displayable_panel(){
+		// tries to find a displayable panel
+		// returns a js object that contains the panel's tab element and text name
+		logger.log('looking for an alternate panel...');
+		var displayable = {"panel_tab_element": null,
+						   "selected_panel_text": null
+						   };
+		var panel_tab_element = $('.pDebugPanels ul li').not('.disabled').first();
+		if (panel_tab_element === undefined){
+			return displayable;
+		}
+		var selected_panel_text = panel_tab_element.attr('id');
+		displayable["panel_tab_element"] = panel_tab_element
+		displayable["selected_panel_text"] = selected_panel_text
+		return displayable
+	}
+	function handle_alt_panel(do_cookie){
+		// consolidates finding and showing an alternate panel
+		// if do_cookie is ``true``, will set the new cookie as the default panel
+		logger.log('handling alternate panel...');
+		// set default value to false
+		do_cookie = typeof do_cookie !== 'undefined' ? do_cookie : false;
+		var displayable = get_displayable_panel();
+		if (displayable.panel_tab_element && displayable.selected_panel_text) {
+			show_panel(displayable.panel_tab_element, displayable.selected_panel_text);
+			if (do_cookie){
+				$.cookie(COOKIE_NAME_STICKYPANEL_SELECTED, selected_panel_text);
 			}
 		}
-		// subscribe panels for recording the active panel in a cookie
-		$(".pDebugPanels ul li a").click(function() {
-			var panel_selected = $(this).attr('id');
-			if (panel_selected){
-				$.cookie(COOKIE_NAME_STICKYPANEL_SELECTED, panel_selected);
-			}
-		});
 	}
+	
+	// only run the feature when activated, otherwise let bootstrap sort it out.
+	if (cookied_panel){
+		// activate the panel if it exists and is populated
+		logger.log("Activating Debug Toolbar Panel : ", cookied_panel);
+		// toolbar doesn't seem to use the normal bootstrap integration, so invoke two methods
+		// the issue is that 2 identical #ids are generated : one on the `li`, one of the `li.a`
+		var cookied_panel_tab = $("#"+cookied_panel);
+		if (!cookied_panel_tab.length){
+			logger.log('The toolbar panel is not on this screen');
+			logger.log('I will set a new cookie value if possible...');
+			handle_alt_panel(1);
+		} else {
+			if (cookied_panel_tab.hasClass('disabled')){
+				logger.log('The toolbar panel is disabled on this view.');
+				handle_alt_panel(0);
+			} else {
+				show_panel(cookied_panel_tab, cookied_panel);
+			}
+		}
+	} else {
+		logger.log('No cookied panel detected');
+		handle_alt_panel(0);
+	}
+
+	// subscribe panels for recording the active panel in a cookie
+	$(".pDebugPanels ul li a").click(function() {
+		var selected_panel_text = $(this).attr('id');
+		if (selected_panel_text){
+			$.cookie(COOKIE_NAME_STICKYPANEL_SELECTED, selected_panel_text);
+		}
+	});
 
 });
