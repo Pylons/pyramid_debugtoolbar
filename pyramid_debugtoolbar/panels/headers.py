dc2011109723b49437d741a56c8f5a8aab46260a
crea <corehack@users.noreply.github.com>
2017-05-27 23:38:03 +0800

    Defer response headers processing to finished callback

--- pyramid_debugtoolbar/panels/headers.py
+++ pyramid_debugtoolbar/panels/headers.py
@@ -15,16 +15,29 @@ class HeaderDebugPanel(DebugPanel):
     nav_title = title
 
     def __init__(self, request):
+        self.request = request
         self.request_headers = [
             (text_(k), text_(v)) for k, v in sorted(request.headers.items())
         ]
 
     def process_response(self, response):
+        def finished_callback(request):
+            self.process_response_deferred(response)
+
+        def prepare_finished_callback(request):
+            """ Best effort to be the last in case there are other callbacks
+            mutating the response """
+            self.request.add_finished_callback(finished_callback)
+
+        self.request.add_finished_callback(prepare_finished_callback)
+        self.data = {'request_headers': self.request_headers,
+                     'response_headers': []}
+
+    def process_response_deferred(self, response):
         response_headers = [
             (text_(k), text_(v)) for k, v in sorted(response.headerlist)
         ]
-        self.data = {'request_headers': self.request_headers,
-                     'response_headers': response_headers}
+        self.data['response_headers'] = response_headers
 
 def includeme(config):
     config.add_debugtoolbar_panel(HeaderDebugPanel)
