6412c5f06ed3a4f8f01bb20ea9f5c884f1288030
jonathan vanasco <jonathan@2xlp.com>
2022-11-14 15:34:53 -0500

    rename "master" to "main"

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ templates_path = ['.templates']
 # The suffix of source filenames.
 source_suffix = '.rst'
 
-# The master toctree document.
+# The main toctree document.
 master_doc = 'index'
 
 # General substitutions.
